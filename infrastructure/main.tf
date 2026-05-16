338655ade5ff733a5269175caa86a5fc7b2f2e32
Samyam-Pradhan <pradhansamyam71@gmail.com>
2026-05-16 15:26:27 +0545

    Add subnets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,12 +25,74 @@ resource "aws_vpc" "main" {
   }
 }
 
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "sock-shop-public-subnet-1"
+    Environment = "production"
+    "kubernetes.io/role/elb" = "1"
+  }
 }
 
-output "vpc_cidr" {
-  description = "CIDR block of the VPC"
-  value       = aws_vpc.main.cidr_block
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "sock-shop-public-subnet-2"
+    Environment = "production"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name        = "sock-shop-private-subnet-1"
+    Environment = "production"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+# Private Subnet 2 — ap-south-1b
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name        = "sock-shop-private-subnet-2"
+    Environment = "production"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_1_id" {
+  value = aws_subnet.public_1.id
+}
+
+output "public_subnet_2_id" {
+  value = aws_subnet.public_2.id
+}
+
+output "private_subnet_1_id" {
+  value = aws_subnet.private_1.id
+}
+
+output "private_subnet_2_id" {
+  value = aws_subnet.private_2.id
 }
