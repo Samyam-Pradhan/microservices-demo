e0149aa46c725101cbdc9722fa31850515d26d8c
Samyam-Pradhan <pradhansamyam71@gmail.com>
2026-05-16 15:12:30 +0545

    Initialize terraform and create vpc

---
+++ infrastructure/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "sock-shop-vpc"
+    Environment = "production"
+    Project     = "sock-shop"
+  }
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
